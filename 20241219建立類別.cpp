a82916d0ab2912f0b3a45abfa26f5481d077eef1
17rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr <313017@s.lhvs.tyc.edu.tw>
2024-12-19 15:28:41 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -5,9 +5,19 @@ class myTime{
 	public:
 	int hour;
 	int minutes;
-	
+	void pnintTime();
 };
-
+void myTime::pnintTime(){ 
+    cout<<hour<<":"<<minute<<"\n";
+} 
 int main(){
-
+	myTime now,open;
+	now.hour=9;
+	now.minute=11;
+	cout<<"現在時間:";
+	now.pnintTime();
+	open.hour=6;
+	open.minute=10;
+	cout<<"開張時間:";
+	open.pnintTime();
 }
